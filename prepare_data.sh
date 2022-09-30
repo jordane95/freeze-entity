14b00c1c45521f31d913db6e07184d1acfb0e96b
jordane95 <zehan.li@qq.com>
2022-09-30 11:01:34 +0800

    upload code

---
+++ prepare_data.sh
@@ -0,0 +1,9 @@
+mkdir -p data
+
+cd data
+
+# QNLI
+wget https://dl.fbaipublicfiles.com/glue/data/QNLIv2.zip
+
+unzip QNLIv2.zip
+
---
+++ run_sst2_freeze.sh
@@ -0,0 +1,13 @@
+export TASK_NAME=sst2
+export DATA_PATH=./data/SST-2
+
+python run_glue_no_trainer.py \
+  --model_name_or_path bert-base-uncased \
+  --max_length 128 \
+  --train_file $DATA_PATH/train.tsv \
+  --validation_file $DATA_PATH/dev.tsv \
+  --per_device_train_batch_size 32 \
+  --learning_rate 2e-5 \
+  --num_train_epochs 3 \
+  --freeze_entity \
+  --output_dir ./tmp/freeze/$TASK_NAME/
